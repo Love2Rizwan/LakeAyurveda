c2aae90eb868e33ce26fed1e2bc28ea7757051cc
love2Rizwan <love2rizwan@gmail.com>
2024-02-07 00:51:02 +0530

    starting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 // App.js
 
-import {
-  Route,
-  Routes,
-  BrowserRouter,
-} from "react-router-dom";
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "./Pages/Navbar/Navbar";
 import Home from "./Pages/Home/Home"; // Correct import path
 import AddBlog from "./Components/AddBlog/AddBlog";
 import BlogDetails from "./Components/BlogDetails/BlogDetails";
+import Banner from "./Pages/Banner/Banner";
 
 function App() {
   return (
@@ -16,11 +14,11 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-        <Route path="/" element={<Home/>}/>
-          <Route path="/react-blog" element={<Home/>}/>
-          <Route path="/blogs/add" element={<AddBlog/>}/>
-          <Route path="/blogs/:id" element={<BlogDetails/>}/>
-         
+          <Route path="/" element={<Home />} />
+          <Route path="/react-blog" element={<Home />} />
+          <Route path="/react-banner" element={<Banner />} /> {/* Corrected the path */}
+          <Route path="/blogs/add" element={<AddBlog />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
         </Routes>
       </div>
     </BrowserRouter>
--- frontend/src/Pages/Navbar/Navbar.jsx
+++ frontend/src/Pages/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
+// Navbar.js
+
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css"; // Import the CSS file
 
 const Navbar = () => {
   const [isHidden, setHidden] = useState(true);
@@ -9,78 +12,49 @@ const Navbar = () => {
   };
 
   return (
-    <header className="md:flex md:items-center md:justify-between py-4 pb-0 md:pb-4">
-      <div className="w-4/5 md:w-9/12 mx-auto md:flex md:items-center md:justify-center">
-        <div className="flex items-center justify-between md:justify-center">
-          <div className="flex items-center justify-between mb-4 md:mb-0 md:hidden">
-            <h1 className="leading-none text-2xl">
-              <Link
-                to="/react-blog"
-                className="text-4xl 2xl:text-8xl no-underline font-allison text-primary"
-              >
-                WriteUp
-              </Link>
-            </h1>
-          </div>
-
-          <nav className="md:hidden mb-4" onClick={handleMenu}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </nav>
-        </div>
-        <nav className={`md:block ${isHidden ? "hidden" : "block"}`}>
-          <ul className="list-reset md:flex md:items-center">
-          <li className="md:ml-4 md:mx-8">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link to="/react-blog" className="navbar-brand">
+          WriteUp
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={handleMenu}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isHidden ? "" : "show"}`}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
               <Link to="/" className="nav-link">
                 Home
               </Link>
             </li>
-            <li className="md:ml-4 md:mx-8">
+            <li className="nav-item">
               <Link to="/blogs/add" className="nav-link">
                 New Blog
               </Link>
             </li>
-            <li className="md:ml-4 md:mx-8">
+            <li className="nav-item">
               <Link to="/react-blog" className="nav-link">
                 Blogs
               </Link>
             </li>
-            <div className="items-center justify-between mb-4 md:mb-0 md:mx-8 hidden md:inline">
-              <h1 className="leading-none text-2xl">
-                <Link
-                  to="/react-blog"
-                  className="text-4xl 2xl:text-8xl no-underline font-allison text-primary"
-                >
-                  WriteUp
-                </Link>
-              </h1>
-            </div>
-            <li className="md:ml-4 md:mx-8">
+            <li className="nav-item">
               <Link to="/about" className="nav-link">
                 About
               </Link>
             </li>
-            <li className="md:ml-4">
+            <li className="nav-item">
               <Link to="/contact" className="nav-link">
                 Contact
               </Link>
             </li>
           </ul>
-        </nav>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
